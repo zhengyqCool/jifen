fabc7075338434ef065b0ef96383b7635d8890a0
Striking <21967223+zhengyqCool@users.noreply.github.com>
2018-08-05 18:21:12 +0800

    tongbu

--- home/cache/template/index/index.tpl.php
+++ home/cache/template/index/index.tpl.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>首页</title>
-    <link rel="stylesheet" href="<?php echo __PUBLIC__;?>css/style.css?t=006">
+    <link rel="stylesheet" href="<?php echo __PUBLIC__;?>css/style.css?t=007">
 </head>
 <body>
     <div class="model-bg"></div>
